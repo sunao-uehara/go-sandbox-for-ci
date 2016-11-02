54e6345457c9ab2e352dda903814699564dca381
Sunao Uehara <uehara.sunao@gmail.com>
2016-11-02 13:38:14 -0700

    add GetProduct test

--- products/product_factory_test.go
+++ products/product_factory_test.go
@@ -8,3 +8,17 @@ func TestRegister(t *testing.T) {
 	// redeclare p1
 	Register("p1", NewP1())
 }
+
+func TestGetProduct(t *testing.T) {
+	// happay path
+	p, err := GetProduct("p1")
+	if p.Name() != "P1" {
+		t.Error("invalid product")
+	}
+
+	// sad path
+	_, err = GetProduct("p3")
+	if err == nil {
+		t.Error("")
+	}
+}
